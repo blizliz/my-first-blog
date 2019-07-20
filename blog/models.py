fec7e3e7349914bbe4b043d9cff395ce481dbbb7
blizliz <lizabezlutskaya@gmail.com>
2019-07-20 23:34:28 +0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+import os
+
+
+def get_image_path(instance, filename):
+    return os.path.join('images', str(instance.id), filename)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+
+def __str__(self):
+    return self.title
